93b751a808fd340c59852e99443cc721b6cda1f6
Gergely Koloszar <gergely.koloszar@gmail.com>
2022-03-03 19:00:52 +0100

    Add ship disassembly function to menu

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,53 @@
 mod models;
 
 use dialoguer::{theme::ColorfulTheme, Input, Select};
+use std::borrow::Borrow;
 
 fn main() {
-    let options = &["Exit", "New Ship"];
+    let mut shipyard = Vec::new();
 
-    let selection = Select::with_theme(&ColorfulTheme::default())
-        .with_prompt("What do you want to do?")
-        .default(0)
-        .items(&options[..])
-        .interact()
-        .unwrap();
+    let options = &["Exit", "New Ship", "List Ships", "Disassemble ship"];
 
-    match options[selection] {
-        "New Ship" => {
-            let name: String = Input::with_theme(&ColorfulTheme::default())
-                .with_prompt("Ships name?")
-                .interact_text()
-                .unwrap();
-            let rocket = models::Ship::new(name);
-            println!("{}, was created!", rocket.name);
+    loop {
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("What do you want to do?")
+            .default(0)
+            .items(&options[..])
+            .interact()
+            .unwrap();
+
+        match options[selection] {
+            "New Ship" => {
+                let name: String = Input::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Ships name?")
+                    .interact_text()
+                    .unwrap();
+
+                let rocket = models::Ship::new(name);
+                println!("{} was created!", rocket.name);
+                shipyard.push(rocket);
+            }
+            "List Ships" => {
+                for ship in &shipyard {
+                    println!("Name: {}", ship.name);
+                }
+            }
+            "Disassemble ship" => {
+                let ship_list: Vec<String> =
+                    shipyard.iter().map(|ship| ship.name.clone()).collect();
+
+                let selection = Select::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Select a ship to disassemble:")
+                    .items(ship_list.as_slice())
+                    .interact()
+                    .unwrap();
+
+                println!("{} was disassembled!", shipyard.remove(selection).name);
+            }
+            _ => {
+                println!("Bye! :)");
+                break;
+            }
         }
-        _ => (),
     }
 }
